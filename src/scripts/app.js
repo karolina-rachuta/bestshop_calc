21a1580383dd77d43054759722141eda6dac9ccb
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-06-16 12:21:49 +0400

    getting value from input and display it

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,7 +3,29 @@ const inputOrdersQuantityRef = document.querySelector('.input_orders');
 const inputPackageRef = document.querySelector('.input_package');
 const selectOptionsRef = document.querySelector('.select_options');
 
+const productLineRef = document.querySelector('.calculator_products-line');
+const numberProductLineRef = document.querySelector('.product-line_number');
+const ordersLineRef = document.querySelector('.calculator_orders-line');
+const numberOrderLineRef = document.querySelector('.order-line_number');
+
+inputProductsQuantityRef.addEventListener("change", () => {
+    productLineRef.style.visibility = "hidden" ? productLineRef.style.display = "visible" : productLineRef.style.display = "hidden";
+    const inputText = inputProductsQuantityRef.value;
+    numberProductLineRef.textContent = inputText;
+})
+
+inputOrdersQuantityRef.addEventListener("change", () => {
+    ordersLineRef.style.visibility = "hidden" ? ordersLineRef.style.display = "visible" : ordersLineRef.style.display = "hidden";
+})
 
 inputPackageRef.addEventListener("click", () => {
-    selectOptionsRef.style.display = "none" ? selectOptionsRef.style.display = "block" : selectOptionsRef.style.display = "none";
+    if (selectOptionsRef.style.display === "none") {
+        selectOptionsRef.style.display = "block"
+    } else selectOptionsRef.style.display = "none"
 })
+
+
+
+function toogleDisplay(selector) {
+    document.querySelector('.selector').style.display === "none" ? document.querySelector('.selector').style.display === "block" : document.querySelector('.selector').style.display === "none"
+}
