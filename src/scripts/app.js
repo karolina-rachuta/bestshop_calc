84be43f8a7731dc0c0d896324036d0bd4c356330
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-07-25 13:34:06 +0400

    modified css, created object with all options, modified product line

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,26 +10,61 @@ const ordersLineRef = document.querySelector('.calculator_orders-line');
 const numberOrderLineRef = document.querySelector('.order-line_number');
 const orderLineResultRef = document.querySelector('.order-line_result');
 
-const packageOption = [{
-        name: 'basic',
-        price: '$20'
+const packageOptions = [{
+        name: 'products',
+        price: 0.5,
+        quanity: 0,
+        total: function () {
+            return this.price * this.quanity
+        }
     },
     {
-        name: 'proffesional',
-        price: '$40'
+        name: 'orders',
+        price: 0.25,
+        quanity: inputOrdersQuantityRef.value,
+        total: function () {
+            return this.price * this.quanity
+        }
     },
     {
-        name: 'premium',
-        price: '$60'
-    }
+        name: 'Basic',
+        price: '$20',
+        selected: false
+    },
+    {
+        name: 'Professional',
+        price: '$40',
+        selected: false
+    },
+    {
+        name: 'Premium',
+        price: '$60',
+        selected: false
+    },
+    {
+        name: 'Accounting',
+        price: '$35',
+        selected: false
+    },
+    {
+        name: 'Terminal',
+        price: '$5',
+        selected: false
+    },
 ]
 
 inputProductsQuantityRef.addEventListener("change", () => {
-    productLineRef.style.visibility = "hidden" ? productLineRef.style.display = "visible" : productLineRef.style.display = "hidden";
-    const inputText = inputProductsQuantityRef.value;
-    numberProductLineRef.textContent = inputText;
-    const result = inputText * 0.5;
-    productLineResultRef.textContent = result;
+        const inputText = inputProductsQuantityRef.value;
+        console.log(inputText)
+        packageOptions[0].quanity = inputText;
+        numberProductLineRef.textContent = inputText;
+        const result = packageOptions[0].total();
+        productLineResultRef.textContent = result;
+        productLineRef.style.visibility = "visible"
+
+        if (!inputProductsQuantityRef.value) {
+            productLineRef.style.visibility = "hidden"
+        }
 })
 
 inputOrdersQuantityRef.addEventListener("change", () => {
@@ -60,8 +95,8 @@ inputPackageRef.addEventListener("click", () => {
 
 document.querySelector('.select_option-basic').addEventListener('click', () => {
     const spans = document.querySelectorAll('.calculator_package-line span')
-    spans[1].textContent = packageOption[0].name;
-    spans[2].textContent = packageOption[0].price;
+    spans[1].textContent = packageOptions[0].name;
+    spans[2].textContent = packageOptions[0].price;
     document.querySelector('.calculator_package-line').style.visibility === 'hidden' ? (
         document.querySelector('.calculator_package-line').style.visibility = 'visible'
     ) : (
@@ -71,8 +106,8 @@ document.querySelector('.select_option-basic').addEventListener('click', () => {
 
 document.querySelector('.select_option-professional').addEventListener('click', () => {
     const spans = document.querySelectorAll('.calculator_package-line span');
-    spans[1].textContent = packageOption[1].name;
-    spans[2].textContent = `${packageOption[1].price}`;
+    spans[1].textContent = packageOptions[1].name;
+    spans[2].textContent = packageOptions[1].price;
     document.querySelector('.calculator_package-line').style.visibility === 'hidden' ? (
         document.querySelector('.calculator_package-line').style.visibility = 'visible'
     ) : (
@@ -82,8 +117,8 @@ document.querySelector('.select_option-professional').addEventListener('click', 
 
 document.querySelector('.select_option-premium').addEventListener('click', () => {
     const spans = document.querySelectorAll('.calculator_package-line span');
-    spans[1].textContent = packageOption[2].name;
-    spans[2].textContent = `${packageOption[2].price}`;
+    spans[1].textContent = packageOptions[2].name;
+    spans[2].textContent = packageOptions[2].price;
     document.querySelector('.calculator_package-line').style.visibility === 'hidden' ? (
         document.querySelector('.calculator_package-line').style.visibility = 'visible'
     ) : (
@@ -94,3 +129,19 @@ document.querySelector('.select_option-premium').addEventListener('click', () =>
 function toogleDisplay(selector) {
     document.querySelector('.selector').style.display === "none" ? document.querySelector('.selector').style.display === "block" : document.querySelector('.selector').style.display === "none"
 }
+
+document.querySelectorAll('.checkbox')[0].addEventListener('click', () => {
+    document.querySelectorAll('.checkbox')[0].checked === true ? document.querySelector('.calculator_accounting-line').style.visibility = 'visible' : document.querySelector('.calculator_accounting-line').style.visibility = 'hidden';
+})
+
+document.querySelectorAll('.checkbox')[1].addEventListener('click', () => {
+    if (document.querySelectorAll('.checkbox')[1].checked === true) {
+        document.querySelector('.calculator_terminal-line').style.visibility = 'visible'
+        document.querySelector('.total_line').style.visibility = 'visible'
+    } else {
+        document.querySelector('.calculator_terminal-line').style.visibility = 'hidden';
+    }
+})
+
+
+document.querySelector('.total_line').style.visibility = 'hidden'
